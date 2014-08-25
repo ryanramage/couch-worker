435bf79e1570e2b6ba95e54090b641b30e512bcd
Caolan McMahon <caolan@caolanmcmahon.com>
2014-08-25 13:50:32 +0100

    add basic integration test (base on couch-worker-photos)

---
+++ test/integration.js
@@ -0,0 +1,84 @@
+var MultiCouch = require('multicouch');
+var couchr = require('highland-couchr');
+var pretape = require('pre-tape');
+var rimraf = require('rimraf');
+var mkdirp = require('mkdirp');
+var exampleworker = require('./lib/example-worker');
+var exec = require('child_process').exec;
+var _ = require('highland');
+
+
+var COUCH_PORT = 5989;
+var COUCH_URL = 'http://localhost:' + COUCH_PORT;
+var COUCH_DIR = __dirname + '/testdb';
+
+
+var couch = new MultiCouch({
+    prefix: COUCH_DIR,
+    port: COUCH_PORT,
+    respawn: false
+});
+
+var test = pretape({
+    setup: function (t, done) {
+      // make sure we kill couch before starting it up again:
+      var cmd = 'pkill -fu ' + process.env.USER + ' ' + COUCH_DIR;
+      exec(cmd, function (err, stdout, stderr) {
+        // delete any existing test database from previous runs
+        rimraf.sync(COUCH_DIR);
+        mkdirp.sync(COUCH_DIR);
+        couch.on('error', function (err) {
+          console.error('CouchDB errored: %s', err);
+        });
+        couch.once('start', function () {
+          // give couch time to start accepting requests
+          setTimeout(function () {
+            couchr.put(COUCH_URL + '/example', {}).apply(function () {
+              done();
+            });
+          }, 2000);
+        });
+        couch.start();
+      });
+    },
+    teardown: function (t) {
+      couch.stop();
+    }
+})
+
+
+test('check couchdb started and example database created', function (t) {
+  couchr.get(COUCH_URL + '/example', {}).apply(function (x) {
+    t.equal(x.body.db_name, 'example');
+    t.end();
+  });
+});
+
+test('pick up non-migrated documents from couchdb', function (t) {
+  var store = {};
+  var config = {
+    name: 'couch-worker-example',
+    database: COUCH_URL + '/example',
+  };
+
+  var worker = exampleworker.start(config);
+  var migrated_doc = require('./fixtures/migrated.json').doc;
+  var notmigrated_doc = require('./fixtures/notmigrated.json').doc;
+  delete notmigrated_doc._rev;
+
+  var url = COUCH_URL + '/example/' + notmigrated_doc._id;
+
+  couchr.put(url, notmigrated_doc).apply(function () {
+    setTimeout(function () {
+      couchr.get(url, {}).apply(function (res) {
+        var doc = res.body;
+        t.equal(doc._rev.substr(0, 2), '2-', '_rev should only be two');
+        delete doc._rev;
+        delete migrated_doc._rev;
+        t.deepEqual(doc, migrated_doc);
+        worker.stop();
+        t.end();
+      });
+    }, 2000);
+  });
+});
---
+++ test/lib/example-worker.js
@@ -0,0 +1,20 @@
+var worker = require('../../index');
+
+module.exports = worker.createWorker(function (config) {
+    var api = {};
+
+    api.ignored = function (doc) {
+        return !doc.hasOwnProperty('a') || !doc.hasOwnProperty('b');
+    };
+
+    api.migrated = function (doc) {
+        return doc.hasOwnProperty('total');
+    };
+
+    api.migrate = function (doc, callback) {
+        doc.total = doc.a + doc.b;
+        return callback(null, doc);
+    };
+
+    return api;
+});
